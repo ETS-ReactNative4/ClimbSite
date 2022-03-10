af75d6a01af4e0ef2de81d07e81f3cee7f119387
joe rizk <joerizk2@hotmail.com>
2022-03-10 16:22:14 +0200

    create all user models

--- climbsite-backend/climbers/models.py
+++ climbsite-backend/climbers/models.py
@@ -1,7 +1,9 @@
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from crags.models import Route, Crag
 
 class CustomManager(BaseUserManager):
     def create_user(self, email, full_name, password,dob, **other_fields):
@@ -33,4 +35,39 @@ class User(AbstractBaseUser, PermissionsMixin):
 class UserFollowing(models.Model):
     follower = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
     following = models.ForeignKey(User, related_name="following",on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class ClimbList(models.Model):
+    user = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, related_name="route",on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Ascending(models.Model):
+    user = models.ForeignKey(User, related_name="climber", on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, related_name="route_climbed",on_delete=models.CASCADE)
+    tries = models.IntegerField()
+    very_bad = 1
+    bad = 2
+    average = 3
+    good = 4
+    very_good = 5
+    STATUS = (
+        (very_bad, _('Very ugly route')),
+        (bad, _('Ugly route')),
+        (average, _('Average route')),
+        (good, _('Good route')),
+        (very_good, _('Beautiful route'))
+
+    )    
+    rating = models.IntegerField(
+       choices=STATUS,
+       default=average,
+   )
+    comment = models.TextField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    crag = models.ForeignKey(Crag, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
