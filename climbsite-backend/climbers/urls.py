653de78ef0e603cde7e6fac30a79680463d7aaf9
joe rizk <joerizk2@hotmail.com>
2022-03-10 17:39:44 +0200

    user register view

--- climbsite-backend/climbers/urls.py
+++ climbsite-backend/climbers/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserCreate
 
 urlpatterns = [
-
+    path('register', UserCreate.as_view()),
 ]
--- climbsite-backend/climbers/views.py
+++ climbsite-backend/climbers/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+User = get_user_model()
+
+class UserCreate(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
