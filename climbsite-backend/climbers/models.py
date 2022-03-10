2374bb9d5920039b80615415e2d3d41adbde30cf
joe rizk <joerizk2@hotmail.com>
2022-03-10 15:21:49 +0200

    custom manager for custom user model

--- climbsite-backend/climbers/models.py
+++ climbsite-backend/climbers/models.py
@@ -3,6 +3,17 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+class CustomManager(BaseUserManager):
+    def create_user(self, email, full_name, password,dob, **other_fields):
+        
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+        
+        email = self.normalize_email(email)
+        user = self.model(email = email, full_name=full_name,dob = dob, **other_fields )
+        user.set_password(password)
+        user.save()
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     
@@ -14,7 +25,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     start_date = models.DateTimeField(default=timezone.now)
 
-    # objects = CustomManager()
+    objects = CustomManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']
