5706533408f8b64e44cac192dfa05bc36298fdb0
joe rizk <joerizk2@hotmail.com>
2022-03-10 15:37:13 +0200

    created sector and route models

--- climbsite-backend/crags/models.py
+++ climbsite-backend/crags/models.py
@@ -11,3 +11,14 @@ class Crag(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     created_on = models.DateTimeField(auto_now_add=True)
+
+class Sector(models.Model):
+    crag = models.ForeignKey(Crag, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+
+class Route(models.Model):
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    grade = models.CharField(max_length=3)
+    slug = models.SlugField(unique=True, max_length=255)
