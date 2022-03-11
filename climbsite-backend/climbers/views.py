f5237107aa9bdf5eeaa228ecbad52b2e97b2b1aa
joe rizk <joerizk2@hotmail.com>
2022-03-11 15:00:52 +0200

    views optimization by using generics

--- climbsite-backend/climbers/views.py
+++ climbsite-backend/climbers/views.py
@@ -3,16 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import generics
 
 
 User = get_user_model()
 
-class UserCreate(APIView):
+class UserCreate(generics.CreateAPIView):
 
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
