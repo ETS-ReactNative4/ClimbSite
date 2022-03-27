ee98caf6feb20a7973a0a603bd2ff30cad1efbb3
joe rizk <joerizk2@hotmail.com>
2022-03-27 19:39:16 +0300

    get all user followings posts linking api

--- climbsite-frontend/ClimbSite/pages/Home.js
+++ climbsite-frontend/ClimbSite/pages/Home.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { styles } from "../styles";
 import { StatusBar } from "expo-status-bar";
+import axios from "axios";
 import {
   Text,
   View,
@@ -15,139 +16,159 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { AntDesign } from "@expo/vector-icons";
 import Header from "../components/Header";
 import Posts from "../components/Posts";
+import { AuthContext } from "../context/userContext";
 
 export default function Home({ navigation }) {
   const { height } = useWindowDimensions();
-  const [posts, setPost] = useState([
-    {
-      id: 1,
-      user: {
-        id: 8,
-        full_name: "Cyril Asmar",
-        email: "cyro@hotmail.com",
-        dob: "1990-12-04",
-      },
-      route: {
-        id: 3,
-        name: "For Charlotte",
-        sector: {
-          id: 1,
-          name: "sector 1",
-          crag: {
-            id: 2,
-            name: "beit merry",
-            description: "it's bet mery",
-            conditions: "hard boldery",
-            gear: "60m rope",
-            longitude: "448.484000",
-            latitude: "841.286000",
-          },
-        },
-        grade: "5c",
-      },
-      tries: 5,
-      rating: 7,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-      date: "2022-03-14",
-    },
-    {
-      id: 2,
-      user: {
-        id: 8,
-        full_name: "Cyril Asmar",
-        email: "cyro@hotmail.com",
-        dob: "1990-12-04",
-      },
-      route: {
-        id: 3,
-        name: "For Charlotte",
-        sector: {
-          id: 1,
-          name: "sector 1",
-          crag: {
-            id: 2,
-            name: "beit merry",
-            description: "it's bet mery",
-            conditions: "hard boldery",
-            gear: "60m rope",
-            longitude: "448.484000",
-            latitude: "841.286000",
-          },
-        },
-        grade: "5c",
-      },
-      tries: 5,
-      rating: 7,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-      date: "2022-03-14",
-    },
-    {
-      id: 3,
-      user: {
-        id: 8,
-        full_name: "Cyril Asmar",
-        email: "cyro@hotmail.com",
-        dob: "1990-12-04",
-      },
-      route: {
-        id: 3,
-        name: "For Charlotte",
-        sector: {
-          id: 1,
-          name: "sector 1",
-          crag: {
-            id: 2,
-            name: "beit merry",
-            description: "it's bet mery",
-            conditions: "hard boldery",
-            gear: "60m rope",
-            longitude: "448.484000",
-            latitude: "841.286000",
-          },
-        },
-        grade: "5c",
-      },
-      tries: 5,
-      rating: 7,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-      date: "2022-03-14",
-    },
-    {
-      id: 4,
-      user: {
-        id: 8,
-        full_name: "Cyril Asmar",
-        email: "cyro@hotmail.com",
-        dob: "1990-12-04",
-      },
-      route: {
-        id: 3,
-        name: "For Charlotte",
-        sector: {
-          id: 1,
-          name: "sector 1",
-          crag: {
-            id: 2,
-            name: "beit merry",
-            description: "it's bet mery",
-            conditions: "hard boldery",
-            gear: "60m rope",
-            longitude: "448.484000",
-            latitude: "841.286000",
-          },
-        },
-        grade: "5c",
-      },
-      tries: 5,
-      rating: 7,
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-      date: "2022-03-14",
-    },
-  ]);
+  const [authState, setAuthState] = useContext(AuthContext);
+  const url = "http://192.168.1.54:7000/api/climbers/get_ascents";
+  async function getPosts() {
+    const token = authState.token;
+
+    try {
+      const response = await axios.get(url, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data_received = await response.data;
+      setPost(data_received);
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+  useEffect(() => {
+    getPosts();
+  }, []);
+  const [posts, setPost] = useState();
+  // const [posts, setPost] = useState([
+  //   {
+  //     id: 1,
+  //     user: {
+  //       id: 8,
+  //       full_name: "Cyril Asmar",
+  //       email: "cyro@hotmail.com",
+  //       dob: "1990-12-04",
+  //     },
+  //     route: {
+  //       id: 3,
+  //       name: "For Charlotte",
+  //       sector: {
+  //         id: 1,
+  //         name: "sector 1",
+  //         crag: {
+  //           id: 2,
+  //           name: "beit merry",
+  //           description: "it's bet mery",
+  //           conditions: "hard boldery",
+  //           gear: "60m rope",
+  //           longitude: "448.484000",
+  //           latitude: "841.286000",
+  //         },
+  //       },
+  //       grade: "5c",
+  //     },
+  //     tries: 5,
+  //     rating: 7,
+  //     comment:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     date: "2022-03-14",
+  //   },
+  //   {
+  //     id: 2,
+  //     user: {
+  //       id: 8,
+  //       full_name: "Cyril Asmar",
+  //       email: "cyro@hotmail.com",
+  //       dob: "1990-12-04",
+  //     },
+  //     route: {
+  //       id: 3,
+  //       name: "For Charlotte",
+  //       sector: {
+  //         id: 1,
+  //         name: "sector 1",
+  //         crag: {
+  //           id: 2,
+  //           name: "beit merry",
+  //           description: "it's bet mery",
+  //           conditions: "hard boldery",
+  //           gear: "60m rope",
+  //           longitude: "448.484000",
+  //           latitude: "841.286000",
+  //         },
+  //       },
+  //       grade: "5c",
+  //     },
+  //     tries: 5,
+  //     rating: 7,
+  //     comment:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     date: "2022-03-14",
+  //   },
+  //   {
+  //     id: 3,
+  //     user: {
+  //       id: 8,
+  //       full_name: "Cyril Asmar",
+  //       email: "cyro@hotmail.com",
+  //       dob: "1990-12-04",
+  //     },
+  //     route: {
+  //       id: 3,
+  //       name: "For Charlotte",
+  //       sector: {
+  //         id: 1,
+  //         name: "sector 1",
+  //         crag: {
+  //           id: 2,
+  //           name: "beit merry",
+  //           description: "it's bet mery",
+  //           conditions: "hard boldery",
+  //           gear: "60m rope",
+  //           longitude: "448.484000",
+  //           latitude: "841.286000",
+  //         },
+  //       },
+  //       grade: "5c",
+  //     },
+  //     tries: 5,
+  //     rating: 7,
+  //     comment:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     date: "2022-03-14",
+  //   },
+  //   {
+  //     id: 4,
+  //     user: {
+  //       id: 8,
+  //       full_name: "Cyril Asmar",
+  //       email: "cyro@hotmail.com",
+  //       dob: "1990-12-04",
+  //     },
+  //     route: {
+  //       id: 3,
+  //       name: "For Charlotte",
+  //       sector: {
+  //         id: 1,
+  //         name: "sector 1",
+  //         crag: {
+  //           id: 2,
+  //           name: "beit merry",
+  //           description: "it's bet mery",
+  //           conditions: "hard boldery",
+  //           gear: "60m rope",
+  //           longitude: "448.484000",
+  //           latitude: "841.286000",
+  //         },
+  //       },
+  //       grade: "5c",
+  //     },
+  //     tries: 5,
+  //     rating: 7,
+  //     comment:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     date: "2022-03-14",
+  //   },
+  // ]);
 
   const [star, setStar] = useState(
     <AntDesign name="star" size={15} color="black" />
