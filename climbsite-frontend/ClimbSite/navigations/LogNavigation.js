f5d558370e09355e17312922c1aead9b71611835
joe rizk <joerizk2@hotmail.com>
2022-03-29 22:42:04 +0300

    provide crag context to log stack

--- climbsite-frontend/ClimbSite/navigations/LogNavigation.js
+++ climbsite-frontend/ClimbSite/navigations/LogNavigation.js
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { styles } from "../styles";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -10,23 +10,35 @@ import { Provider } from "react-native-paper";
 import Log from "../pages/Log";
 import Profile from "../pages/Profile";
 import Sectors from "../pages/Sectors";
+import { CragContext } from "../context/cragContext";
 
 const layout = createNativeStackNavigator();
 
 export default function LogNavigation() {
+  const [cragState, setCragState] = useState({
+    id: "",
+    name: "",
+    description: "",
+    conditions: "",
+    gear: "",
+    longitude: "",
+    latitude: "",
+  });
   return (
     <Provider>
-      <layout.Navigator
-        initialRouteName="Log"
-        screenOptions={{ headerShown: false }}
-      >
-        <layout.Screen name="Log" component={Log} />
-        <layout.Screen name="LogClimb" component={LogClimb} />
-        <layout.Screen name="LogEvent" component={LogEvent} />
-        <layout.Screen name="SearchEvent" component={SearchEvent} />
-        <layout.Screen name="Profile" component={Profile} />
-        <layout.Screen name="Sectors" component={Sectors} />
-      </layout.Navigator>
+      <CragContext.Provider value={[cragState, setCragState]}>
+        <layout.Navigator
+          initialRouteName="Log"
+          screenOptions={{ headerShown: false }}
+        >
+          <layout.Screen name="Log" component={Log} />
+          <layout.Screen name="LogClimb" component={LogClimb} />
+          <layout.Screen name="LogEvent" component={LogEvent} />
+          <layout.Screen name="SearchEvent" component={SearchEvent} />
+          <layout.Screen name="Profile" component={Profile} />
+          <layout.Screen name="Sectors" component={Sectors} />
+        </layout.Navigator>
+      </CragContext.Provider>
     </Provider>
   );
 }
