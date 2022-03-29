01849de2ac39a53afec89bb510ae8d969080375d
joe rizk <joerizk2@hotmail.com>
2022-03-29 22:40:03 +0300

    linked get all crags in log climb screen

--- climbsite-frontend/ClimbSite/pages/LogClimb.js
+++ climbsite-frontend/ClimbSite/pages/LogClimb.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { styles } from "../styles";
 import { StatusBar } from "expo-status-bar";
 import {
@@ -11,50 +11,68 @@ import {
   Image,
   ScrollView,
 } from "react-native";
+import axios from "axios";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FlatList } from "react-native-gesture-handler";
 import ProfileHeader from "../components/ProfileHeader";
+import { CragContext } from "../context/cragContext";
 
 export default function LogClimb({ navigation }) {
   const { height } = useWindowDimensions();
-  const [crag, setCrag] = useState([
-    {
-      id: 2,
-      name: "Beit merry",
-      description: "it's bet mery",
-      conditions: "hard boldery",
-      gear: "60m rope",
-      longitude: 33.890536626710244,
-      latitude: 35.489303601542964,
-    },
-    {
-      id: 3,
-      name: "Tannourine",
-      description: "it's Tannourine",
-      conditions: "hard tannourine",
-      gear: "80m rope",
-      longitude: 34.890536626710244,
-      latitude: 36.489303601542964,
-    },
-    {
-      id: 7,
-      name: "Beit merry",
-      description: "it's bet mery",
-      conditions: "hard boldery",
-      gear: "60m rope",
-      longitude: 33.890536626710244,
-      latitude: 35.489303601542964,
-    },
-    {
-      id: 8,
-      name: "Tannourine",
-      description: "it's Tannourine",
-      conditions: "hard tannourine",
-      gear: "80m rope",
-      longitude: 34.890536626710244,
-      latitude: 36.489303601542964,
-    },
-  ]);
+  const [cragState, setCragState] = useContext(CragContext);
+
+  const url = "http://192.168.1.54:7000/api/crags/";
+  async function getCrag() {
+    try {
+      const response = await axios.get(url);
+      const data_received = await response.data;
+      setCrag(data_received);
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+  useEffect(() => {
+    getCrag();
+  }, []);
+  const [crag, setCrag] = useState();
+  // const [crag, setCrag] = useState([
+  //   {
+  //     id: 2,
+  //     name: "Beit merry",
+  //     description: "it's bet mery",
+  //     conditions: "hard boldery",
+  //     gear: "60m rope",
+  //     longitude: 33.890536626710244,
+  //     latitude: 35.489303601542964,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Tannourine",
+  //     description: "it's Tannourine",
+  //     conditions: "hard tannourine",
+  //     gear: "80m rope",
+  //     longitude: 34.890536626710244,
+  //     latitude: 36.489303601542964,
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "Beit merry",
+  //     description: "it's bet mery",
+  //     conditions: "hard boldery",
+  //     gear: "60m rope",
+  //     longitude: 33.890536626710244,
+  //     latitude: 35.489303601542964,
+  //   },
+  //   {
+  //     id: 8,
+  //     name: "Tannourine",
+  //     description: "it's Tannourine",
+  //     conditions: "hard tannourine",
+  //     gear: "80m rope",
+  //     longitude: 34.890536626710244,
+  //     latitude: 36.489303601542964,
+  //   },
+  // ]);
   const [favorites, setFavorites] = useState([
     {
       id: 4,
@@ -131,48 +149,59 @@ export default function LogClimb({ navigation }) {
         <Text style={{ marginLeft: 45, marginVertical: 20, fontSize: 20 }}>
           Others
         </Text>
-        {crag.map((item) => {
-          return (
-            <View
-              key={item.id}
-              style={{
-                flex: 1,
-                justifyContent: "flex-end",
-                alignItems: "center",
-              }}
-            >
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate("Sectors");
+        {crag &&
+          crag.map((item) => {
+            return (
+              <View
+                key={item.id}
+                style={{
+                  flex: 1,
+                  justifyContent: "flex-end",
+                  alignItems: "center",
                 }}
               >
-                <View
-                  style={{
-                    marginVertical: 10,
-                    backgroundColor: "#2F3F4A",
-                    borderRadius: 15,
-                    padding: 20,
-                    width: 320,
-                    alignItems: "center",
-                    flexDirection: "row",
+                <TouchableOpacity
+                  onPress={() => {
+                    setCragState({
+                      id: item.id,
+                      name: item.name,
+                      description: item.description,
+                      conditions: item.conditions,
+                      gear: item.gear,
+                      longitude: item.longitude,
+                      latitude: item.latitude,
+                    });
+
+                    navigation.navigate("Sectors");
                   }}
                 >
-                  <View style={{ flex: 0.3 }}>
-                    <Image
-                      style={{ width: 80, height: 80 }}
-                      source={require("../assets/betmerry.jpg")}
-                    ></Image>
+                  <View
+                    style={{
+                      marginVertical: 10,
+                      backgroundColor: "#2F3F4A",
+                      borderRadius: 15,
+                      padding: 20,
+                      width: 320,
+                      alignItems: "center",
+                      flexDirection: "row",
+                    }}
+                  >
+                    <View style={{ flex: 0.3 }}>
+                      <Image
+                        style={{ width: 80, height: 80 }}
+                        source={require("../assets/betmerry.jpg")}
+                      ></Image>
+                    </View>
+                    <View style={{ flex: 0.8, marginLeft: 20 }}>
+                      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                        {item.name}
+                      </Text>
+                    </View>
                   </View>
-                  <View style={{ flex: 0.8, marginLeft: 20 }}>
-                    <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-                      {item.name}
-                    </Text>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            </View>
-          );
-        })}
+                </TouchableOpacity>
+              </View>
+            );
+          })}
       </ScrollView>
     </View>
   );
