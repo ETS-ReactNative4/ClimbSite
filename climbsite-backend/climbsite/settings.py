e730ccebad58b158d584e659421ab82967b1c1ab
joe rizk <joerizk2@hotmail.com>
2022-04-09 02:09:51 +0300

    fixed database password

--- climbsite-backend/climbsite/settings.py
+++ climbsite-backend/climbsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-eb#-r1rym3ckn0*zn7w#4_eg0h7jh-9mq+c-+y9mae9hm!@nsd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.4','192.168.100.7','192.168.1.5','192.168.1.54','192.168.170.123','192.168.1.6','192.168.1.3','127.0.0.1']
+ALLOWED_HOSTS = ['ec2-18-222-82-216.us-east-2.compute.amazonaws.com','192.168.1.4','192.168.100.7','192.168.1.5','192.168.1.54','192.168.170.123','192.168.1.6','192.168.1.3','127.0.0.1']
 
 
 # Application definition
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'climbsitedb',  
         'USER': 'root',  
-        'PASSWORD': '',  
+        'PASSWORD': 'MrRobotFsociety2Z<y*C3eD)w>T',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',
     }
